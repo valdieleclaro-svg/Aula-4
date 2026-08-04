89c387ed65f6aa5243362954ed75be3e2197ada8
valdieleclaro-svg <valdiele.claro@escola.pr.gov.br>
2026-08-04 09:01:20 -0300

    Add like button functionality to script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+const botoes = document.querySelectorAll(".btn-like");
 
+botoes.forEach(function (botao) {
+    let curtiu = false;
+
+    botao.addEventListener("click", function () {
+        let texto = botao.querySelector("span");
+        let contador = parseInt(texto.textContent, 10);
+
+        if (!curtiu) {
+            texto.textContent = contador + 1;
+            curtiu = true;
+            botao.classList.add("curtido");
+        } else {
+            texto.textContent = contador - 1;
+            curtiu = false;
+            botao.classList.remove("curtido");
+        }
+    });
+});
